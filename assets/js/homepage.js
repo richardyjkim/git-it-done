73b3e803026e1da0dadfe638167d3d843146e161
Richard <richardyjkim@gmail.com>
2021-03-04 12:14:10 -0800

    optimized js

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,28 +3,30 @@ let nameInputEl = document.querySelector("#username");
 let repoContainerEl = document.querySelector("#repos-container");
 let repoSearchTerm = document.querySelector("#repo-search-term");
 
-let getUserRepos = function(user) {
+let getUserRepos = function (user) {
   // format the github api url
   let apiUrl = "https://api.github.com/users/" + user + "/repos";
-  
+
   // make a reques to the url
-  fetch(apiUrl).then(function(response) {
-    if (response.ok) {
-      response.json().then(function(data){ 
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+
+      console.log(data);
+
+      if (response.ok) {
         displayRepos(data, user);
-        });
       } else {
-      alert ("Error: " + response.statusText);
+        alert("Error: " + data.message);
       }
-    })
-  .catch(function(error){
+    });
+  }).catch(function (error) {
     // notice this `.catch()` getting chained onto the end of the `.then()` method
     alert("Unable to connect to Github")
-    });
-  };
+  });
+};
 
 
-let formSubmitHandler = function(event) {
+let formSubmitHandler = function (event) {
   event.preventDefault();
   let username = nameInputEl.value.trim();
 
@@ -35,44 +37,45 @@ let formSubmitHandler = function(event) {
     alert("Please enter a GutHub username")
   }
   console.log(event);
+  
 };
 
-let displayRepos = function(repos, searchTerm) {
-  if (repos.length === 0){
-    repoContainerEl.textContent = "No repositories found."; 
+let displayRepos = function (repos, searchTerm) {
+  if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
     return;
   }
- repoContainerEl.textContent = "";
- repoSearchTerm.textContent = searchTerm;
- for (let i=0; i < repos.length; i++) {
-  //  format repo name
-  let repoName = repos[i].owner.login + "/" + repos[i].name;
-  
-  // create a container for each repo
-  let repoEl = document.createElement("div");
-  repoEl.classList = "list-item flex-row justify-space-between align-center";
+  repoContainerEl.textContent = "";
+  repoSearchTerm.textContent = searchTerm;
+  for (let i = 0; i < repos.length; i++) {
+    //  format repo name
+    let repoName = repos[i].owner.login + "/" + repos[i].name;
 
-  // create span elemt to hold repository name
-  let titleEl = document.createElement("span");
-  titleEl.textContent = repoName;
+    // create a container for each repo
+    let repoEl = document.createElement("div");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
 
-  // append to container
-  repoEl.appendChild(titleEl)
+    // create span elemt to hold repository name
+    let titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
 
-  // create a status element
-  let statusEl = document.createElement("span");
-  statusEl.classList = "flex-row align-center";
+    // append to container
+    repoEl.appendChild(titleEl)
 
-  if (repos[i].open_issues_count > 0){
-    statusEl.innerHTML =
-    "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count +" issue(s)";
-  } else {
-    statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>"; 
-  }
-  // append to container
-  repoEl.appendChild(statusEl);
-  // append container to the dom
-  repoContainerEl.appendChild(repoEl);
+    // create a status element
+    let statusEl = document.createElement("span");
+    statusEl.classList = "flex-row align-center";
+
+    if (repos[i].open_issues_count > 0) {
+      statusEl.innerHTML =
+        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+    } else {
+      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
+    // append to container
+    repoEl.appendChild(statusEl);
+    // append container to the dom
+    repoContainerEl.appendChild(repoEl);
   }
 };
 
